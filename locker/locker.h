1bc1b13b15c5176ee3a658a452aae4fa4c424367
nailoong <826542246@qq.com>
2025-11-17 16:02:15 +0800

    Refactor: Implemented C++11 semaphore in locker.h and removed legacy wrappers

---
+++ locker/locker.h
@@ -0,0 +1,32 @@
+#ifndef LOCKER_H
+#define LOCKER_H
+
+#include <exception>
+#include <mutex>
+#include <condition_variable>
+
+class sem {
+    public:
+    sem(int value = 0) : count(value) {
+    }
+
+    void post(){
+        std::unique_lock<std::mutex> lock(m_mutex);
+        count ++;
+        m_cond.notify_one();
+    }
+
+    void wait(){
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_cond.wait(lock, [this](){return count > 0;});
+        count --;
+    }
+
+    private:
+    int count;
+    std::mutex m_mutex;
+    std::condition_variable m_cond;
+
+};
+
+#endif
